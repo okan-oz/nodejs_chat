780a8cdfc4df2b267bbcdce850b9c06241de8375
ooz <ooz@ziraatteknoloji.com>
2022-03-12 14:48:23 +0300

    add socket server information --ooz

--- public/main.js
+++ public/main.js
@@ -225,7 +225,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    const message = 'Welcome to Socket.IO Chat – ';
+    const message = 'Socket.IO Test – Socket Hostname:'+ socket.io.engine.hostname + ' Port:'+socket.io.engine.port;
     log(message, {
       prepend: true
     });
